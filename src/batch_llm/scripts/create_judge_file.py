95771e66d6ee4fe6ce9f80967cef2bbf909260e1
fedenanni <nanni.federico@gmail.com>
2024-04-16 17:01:16 +0100

    Add create judge script

---
+++ src/batch_llm/scripts/create_judge_file.py
@@ -0,0 +1,97 @@
+import argparse
+import json
+import os
+
+
+def main():
+    """
+    Generate a file for the judge-llm experiment using the responses from a completed file.
+    """
+    # parse command line arguments
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        "--input-file",
+        "-i",
+        help="Path to the input file containing the responses to judge",
+        type=str,
+        required=True,
+    )
+    parser.add_argument(
+        "--judge-location",
+        "-l",
+        help="`Location of the judge template and settings to be used",
+        type=str,
+        required=True,
+    )
+    parser.add_argument(
+        "--judge", "-j", help="Judge to be used", type=str, default="gemini"
+    )
+
+    parser.add_argument(
+        "--output-folder",
+        "-o",
+        help="Location where the judge file will be created.",
+        type=str,
+        default="./",
+    )
+    args = parser.parse_args()
+
+    input_file = args.input_file
+    judge_location = args.judge_location
+    judge = args.judge
+    output_folder = args.output_folder
+
+    try:
+        with open(input_file, "r", encoding="utf-8") as f:
+            responses = f.readlines()
+    except FileNotFoundError as exc:
+        raise FileNotFoundError(f"Input file '{input_file}' does not exist.") from exc
+
+    try:
+        template_path = os.path.join(judge_location, "template.txt")
+        with open(template_path, "r", encoding="utf-8") as f:
+            template_prompt = f.read()
+    except FileNotFoundError as exc:
+        raise FileNotFoundError(
+            f"Template file '{template_path}' does not exist."
+        ) from exc
+
+    try:
+        judge__settings_path = os.path.join(judge_location, "settings.json")
+        with open(judge__settings_path, "r", encoding="utf-8") as f:
+            judge_settings = json.load(f)
+    except FileNotFoundError as exc:
+        raise FileNotFoundError(
+            f"Settings file '{judge__settings_path}' does not exist."
+        ) from exc
+
+    if judge not in judge_settings:
+        raise ValueError(f"Judge '{judge}' not in judge settings.")
+
+    settings = judge_settings[judge]
+
+    # open output jsonl file and write the judge prompts
+    with open(output_folder + "judge.jsonl", "w", encoding="utf-8") as f:
+        for _, response in enumerate(responses):
+            response = json.loads(response)
+            prompt_id = "judge-" + str(response["id"])
+            prompt = response["prompt"]
+            response = response["response"]
+            judge_prompt = (
+                f"{template_prompt}\nQUESTION: {prompt}\nRESPONSE: {response}\nCLASS:"
+            )
+            f.write(
+                json.dumps(
+                    {
+                        "id": prompt_id,
+                        "prompt": judge_prompt,
+                        "model": judge,
+                        "parameters": settings,
+                    }
+                )
+            )
+            f.write("\n")
+
+
+if __name__ == "__main__":
+    main()
