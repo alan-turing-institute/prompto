3f60a25934daa3b6165925bbb3a3c08f5385a4d1
fedenanni <nanni.federico@gmail.com>
2024-04-19 13:45:14 +0100

    Add ollama to init

--- src/batch_llm/models/__init__.py
+++ src/batch_llm/models/__init__.py
@@ -4,6 +4,7 @@ from batch_llm.models.azure_openai.azure_openai import (
 )
 from batch_llm.models.base import AsyncBaseModel, BaseModel
 from batch_llm.models.gemini.gemini import AsyncGeminiModel, GeminiModel
+from batch_llm.models.ollama.ollama import AsyncOllamaModel
 from batch_llm.models.testing.testing_model import AsyncTestModel, TestModel
 
 MODELS: dict[str, BaseModel] = {
@@ -16,6 +17,7 @@ ASYNC_MODELS: dict[str, AsyncBaseModel] = {
     "azure_openai": AsyncAzureOpenAIModel,
     "gemini": AsyncGeminiModel,
     "test": AsyncTestModel,
+    "ollama": AsyncOllamaModel,
 }
 
 __all__ = ["MODELS", "ASYNC_MODELS"]
