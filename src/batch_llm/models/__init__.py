02b89a6fa848e309a720b7abe5db8efa5a649806
rchan <rchan@turing.ac.uk>
2024-04-19 08:32:12 +0200

    add openai model

--- src/batch_llm/models/__init__.py
+++ src/batch_llm/models/__init__.py
@@ -4,17 +4,20 @@ from batch_llm.models.azure_openai.azure_openai import (
 )
 from batch_llm.models.base import AsyncBaseModel, BaseModel
 from batch_llm.models.gemini.gemini import AsyncGeminiModel, GeminiModel
+from batch_llm.models.openai.openai import AsyncOpenAIModel, OpenAIModel
 from batch_llm.models.testing.testing_model import AsyncTestModel, TestModel
 
 MODELS: dict[str, BaseModel] = {
     "azure_openai": AzureOpenAIModel,
     "gemini": GeminiModel,
+    "openai": OpenAIModel,
     "test": TestModel,
 }
 
 ASYNC_MODELS: dict[str, AsyncBaseModel] = {
     "azure_openai": AsyncAzureOpenAIModel,
     "gemini": AsyncGeminiModel,
+    "openai": AsyncOpenAIModel,
     "test": AsyncTestModel,
 }
 
