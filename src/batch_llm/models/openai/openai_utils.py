46f0ef564a67e44ae5b715243da7532bf28d2f3f
rchan <rchan@turing.ac.uk>
2024-04-30 10:02:37 +0100

    add checks in check_prompt_dict for openai

--- src/batch_llm/models/openai/openai_utils.py
+++ src/batch_llm/models/openai/openai_utils.py
@@ -1,5 +1,6 @@
 import os
 from enum import Enum
+
 from openai.types.chat import ChatCompletion
 from openai.types.completion import Completion
 
@@ -36,6 +37,51 @@ def check_environment_variables() -> list[Exception]:
 
 
 def check_prompt_dict(prompt_dict: dict) -> list[Exception]:
+    issues = []
+
+    # check prompt is of the right type
+    match prompt_dict["prompt"]:
+        case str(_):
+            pass
+        case [str(_)]:
+            pass
+        case [{"role": role, "content": _}, *rest]:
+            if role in ChatRoles and all(
+                [
+                    set(d.keys()) == {"role", "content"} and d["role"] in ChatRoles
+                    for d in rest
+                ]
+            ):
+                pass
+        case _:
+            issues.append(
+                TypeError(
+                    "If model == 'openai', then the prompt must be a str, list[str], or "
+                    "list[dict[str,str]] where the dictionary contains the keys 'role' and "
+                    "'content' only, and the values for 'role' must be one of 'system', 'user' or "
+                    "'assistant'"
+                )
+            )
+
+    # check if the model_name is set as an environment variable if not provided in the prompt_dict
+    if "model_name" not in prompt_dict:
+        req_var = "OPENAI_MODEL_NAME"
+        if req_var not in os.environ:
+            issues.append(
+                ValueError(
+                    f"model_name is not set and environment variable {req_var} is not set"
+                )
+            )
+
+    # if mode is passed, check it is a valid value
+    if "mode" in prompt_dict and prompt_dict["mode"] not in ["chat", "completion"]:
+        issues.append(
+            ValueError(
+                f"Invalid mode value. Must be 'chat' or 'completion', not {prompt_dict['mode']}"
+            )
+        )
+
     # TODO: add checks for prompt_dict["parameters"] being
     # valid arguments for OpenAI API without hardcoding
-    return []
+
+    return issues
