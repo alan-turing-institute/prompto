2429261f0592da10e12d41bc61f709414f82e001
rchan <rchan@turing.ac.uk>
2024-05-17 11:32:49 +0100

    sort ollama prompts by model_name while keeping others the same index

--- src/prompto/experiment_processing.py
+++ src/prompto/experiment_processing.py
@@ -130,6 +130,54 @@ class Experiment:
 
         return grouped_dict
 
+    @staticmethod
+    def _sort_ollama_prompts(prompt_dicts: list[dict]) -> list[dict]:
+        """
+        For a list of prompt dictionaries, sort the dictionaries with "api": "ollama"
+        by the "model_name" key. The rest of the dictionaries are kept in the same order.
+
+        For Ollama API, if the model requested is not currently loaded, the model will be
+        loaded on demand. This can take some time, so it is better to sort the prompts
+        by the model name to reduce the time taken to load the models.
+
+        If no "ollama" dictionaries are present, the original list is returned.
+
+        Parameters
+        ----------
+        prompt_dicts : list[dict]
+            List of dictionaries containing the prompt and other parameters
+            to be sent to the API. Each dictionary must have keys "prompt" and "api".
+
+        Returns
+        -------
+        list[dict]
+            List of dictionaries containing the prompt and other parameters
+            where the "ollama" dictionaries are sorted by the "model_name" key
+        """
+        ollama_indices = [
+            i for i, item in enumerate(prompt_dicts) if item.get("api") == "ollama"
+        ]
+        if len(ollama_indices) == 0:
+            return prompt_dicts
+
+        # sort indices for "ollama" dictionaries
+        sorted_ollama_indices = sorted(
+            ollama_indices, key=lambda i: prompt_dicts[i].get("model_name", "")
+        )
+
+        # create map from original ollama index to sorted index
+        ollama_index_map = {i: j for i, j in zip(ollama_indices, sorted_ollama_indices)}
+
+        # sort data based on the combined indices
+        return [
+            (
+                prompt_dicts[i]
+                if i not in ollama_index_map.keys()
+                else prompt_dicts[ollama_index_map[i]]
+            )
+            for i in range(len(prompt_dicts))
+        ]
+
 
 class ExperimentPipeline:
     """
@@ -294,6 +342,7 @@ class ExperimentPipeline:
             }
             logging.info(f"Queries per model: {queries_per_model}")
 
+            # create tasks for each model which we will run in parallel using asyncio.gather
             tasks = [
                 asyncio.create_task(
                     self.send_requests_retry(
