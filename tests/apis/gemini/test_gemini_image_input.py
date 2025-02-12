d653d4532e2bdb503cb9ba33ecf742f4db24cc9c
fedenanni <nanni.federico@gmail.com>
2025-02-12 17:21:38 +0000

    Clean up tests

--- tests/apis/gemini/test_gemini_image_input.py
+++ tests/apis/gemini/test_gemini_image_input.py
@@ -1,39 +1,23 @@
-import logging
-from unittest.mock import AsyncMock, Mock, patch
-
-import pytest
-from google.generativeai import GenerativeModel
-
-from prompto.apis.gemini import GeminiAPI
-from prompto.settings import Settings
-
-from .test_gemini import DEFAULT_SAFETY_SETTINGS
-
-pytest_plugins = ("pytest_asyncio",)
-
-from prompto.apis.gemini.gemini_utils import parse_parts, parse_parts_value
+from prompto.apis.gemini.gemini_utils import parse_parts_value
 from PIL import Image
 import os
 
 
-@pytest.mark.asyncio
-async def test_parse_parts_value_text():
+def test_parse_parts_value_text():
     part = "text"
     media_folder = "media"
     result = parse_parts_value(part, media_folder)
     assert result == part
 
 
-@pytest.mark.asyncio
-async def test_parse_parts_value_image():
+def test_parse_parts_value_image():
     part = "image"
     media_folder = "media"
     result = parse_parts_value(part, media_folder)
     assert result == part
 
 
-@pytest.mark.asyncio
-async def test_parse_parts_value_image_dict():
+def test_parse_parts_value_image_dict():
     part = {"type": "image", "media": "pantani_giro.jpg"}
     media_folder = "media"
 
@@ -56,9 +40,7 @@ async def test_parse_parts_value_image_dict():
     os.rmdir(media_folder)
 
 
-# add a test for a video input
-@pytest.mark.asyncio
-async def test_parse_parts_value_video():
+def test_parse_parts_value_video():
     part = {"type": "video", "media": "pantani_giro.mp4"}
     media_folder = "media"
 
